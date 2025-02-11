dd1f3c9c97b7110f651aa10d64bc8e6d93d048a3
jestemmiku <contact.mikuu@gmail.com>
2025-02-11 22:17:22 +0100

    Update memes.py

--- api/memes.py
+++ api/memes.py
@@ -27,7 +27,8 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
+        "doMessage": True
+    , # Enable the custom message?
         "message": "SHIT ON 200 PUMP NO SCOPE NIGGA discord.gg/2pm7wnxRnz", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
